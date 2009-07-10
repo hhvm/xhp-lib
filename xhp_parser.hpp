6f7e684fd68d34626cf14d0aaa37c5c09d960c2f
Iain Proctor <iproctor@facebook.com>
2009-07-10 09:08:52 -0800

    xhp build on centos needs stdint.h
    Summary: For some reason, when building xhp on a centos machine, I needed to
             include stdint.h for uint32_t.
    
    Reviewed By: marcel
    
    Test Plan: Built on a centos machine and a fedora machine.
    
    Revert: OK
    
    DiffCamp Revision: 54284

--- xhp_parser.hpp
+++ xhp_parser.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stdint.h>
 #include <stdio.h>
 #include <string>
 #include <stack>
--- xhp_preprocess.hpp
+++ xhp_preprocess.hpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <stdint.h>
 
 enum XHPResult {
   XHPDidNothing,
