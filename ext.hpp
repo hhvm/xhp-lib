82da35963c7ae6e1f718b7f9dcec13b436d9e4da
Marcel Laverdet <marcel@facebook.com>
2010-01-07 12:51:40 -0800

    Add documentation and license information

--- ext.hpp
+++ ext.hpp
@@ -1,3 +1,20 @@
+/*
+  +----------------------------------------------------------------------+
+  | XHP                                                                  |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1998-2009 Zend Technologies Ltd. (http://www.zend.com) |
+  | Copyright (c) 2009 Facebook, Inc. (http://www.facebook.com)          |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 2.00 of the Zend license,     |
+  | that is bundled with this package in the file LICENSE.ZEND, and is   |
+  | available through the world-wide-web at the following url:           |
+  | http://www.zend.com/license/2_00.txt.                                |
+  | If you did not receive a copy of the Zend license and are unable to  |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@zend.com so we can mail you a copy immediately.              |
+  +----------------------------------------------------------------------+
+*/
+
 #pragma once
 #ifdef HAVE_CONFIG_H
 #include "../config.h"
