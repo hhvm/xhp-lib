727cd75f377f9a68645ad7b06149db6accb1fd30
Fred Emmott <fe@fb.com>
2015-01-12 15:03:06 -0800

    Remove PHP5 extension
    This is now available at https://github.com/facebookarchive/xhp-php5-extension/

--- tests/attr-quotes.phpt
+++
@@ -1,9 +0,0 @@
---TEST--
-Quotes in attribute
---FILE--
-<?php
-require 'lib.php';
-$quote = '"';
-echo <x b={$quote}>c</x>;
---EXPECT--
-<x b="&quot;">c</x>
--- tests/namespace-lib.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Extremely simple implementation of XHP client library useful primarily for
-// namespace unit tests.
-
-class :x {
-  public function __construct() {
-    echo '-'.__NAMESPACE__.'-';
-  }
-}
