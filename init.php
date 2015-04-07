99c9755def22c980bc71141d91c3e8d272db55ec
Fred Emmott <fe@fb.com>
2015-04-07 11:57:22 -0700

    Add static autoload map, generated by composer, but to be commited
    Fixes facebook/xhp-lib#129

---
+++ init.php
@@ -0,0 +1,15 @@
+<?hh
+/*
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+// IF YOU ARE USING COMPOSER: use Composer's autoload, and ignore this file.
+
+require_once(__DIR__.'/autoload-map.php');
+spl_autoload_register('Facebook\XHPLib\Autoloader\autoload');
