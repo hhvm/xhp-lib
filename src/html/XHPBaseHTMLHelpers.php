663eb95fc282c40ca38a3766830f29bf02ec00dd
Fred Emmott <fe@fb.com>
2015-04-27 16:25:15 -0700

    anoteher whitespace nit

--- src/html/XHPBaseHTMLHelpers.php
+++ src/html/XHPBaseHTMLHelpers.php
@@ -10,7 +10,7 @@
  *
  */
 
-trait XHPBaseHTMLHelpers implements HasXHPBaseHTMLHelpers{
+trait XHPBaseHTMLHelpers implements HasXHPBaseHTMLHelpers {
   require extends :x:composable-element;
 
   /*
