ffdd82c48ae9c4dd17af1ced2164f05c54d2a6cb
Fred Emmott <fe@fb.com>
2015-05-05 09:46:50 -0700

    add passing tests for contexts

---
+++ tests/ContextsTest.php
@@ -0,0 +1,34 @@
+<?hh
+
+class :test:contexts extends :x:element {
+  protected function render(): XHPRoot {
+    return
+      <div>
+        <p>{$this->getContext('heading')}</p>
+        {$this->getChildren()}
+      </div>;
+  }
+}
+
+class XHPContextsTest extends PHPUnit_Framework_TestCase {
+  public function testContextSimple(): void {
+    $x = <test:contexts />;
+    $x->setContext('heading', 'herp');
+    $this->assertSame('<div><p>herp</p></div>', $x->toString());
+  }
+
+  public function testContextInsideHTMLElement(): void {
+    $x = <div><test:contexts /></div>;
+    $x->setContext('heading', 'herp');
+    $this->assertSame('<div><div><p>herp</p></div></div>', $x->toString());
+  }
+
+  public function testNestedContexts(): void {
+    $x = <test:contexts><test:contexts /></test:contexts>;
+    $x->setContext('heading', 'herp');
+    $this->assertSame(
+      '<div><p>herp</p><div><p>herp</p></div></div>',
+      $x->toString(),
+    );
+  }
+}
