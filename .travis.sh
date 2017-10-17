a40c1aee428ca38d2780788f55da8c7240747652
Fred Emmott <fe@fb.com>
2017-10-17 11:19:53 -0700

    test with experimental shape options

--- .travis.sh
+++ .travis.sh
@@ -8,6 +8,12 @@ hhvm -d hhvm.jit=0 /usr/local/bin/composer install
 
 hh_server --check $(pwd)
 hhvm -d hhvm.jit=0 vendor/bin/phpunit
-if [ $(hhvm --php -r 'echo HHVM_VERSION_ID;' 2>/dev/null) -ge 32002 ]; then
+
+HHVM_VERSION=$(hhvm --php -r 'echo HHVM_VERSION_ID;' 2>/dev/null);
+if [ $HHVM_VERSION -ge 32002 ]; then
   hhvm -d hhvm.php7.all=1 -d hhvm.jit=0 vendor/bin/phpunit tests/
+fi
+if [ $HHVM_VERSION -ge 32200 -a $HHVM_VERSION -lt 32300 ]; then
+  echo enable_experimental_tc_features = optional_shape_field, unknown_fields_shape_is_not_subtype_of_known_fields_shape >> .hhconfig
+  hh_server --check $(pwd)
 fi
