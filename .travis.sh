5c007c9ab236fb03f537f731e219c8a838b72a8c
Fred Emmott <fe@fb.com>
2019-04-26 09:50:29 -0700

    Stop committing composer.lock
    Given HHVM's releasing weekly, these need updating much more frequently,
    and are creating more work than they save.

--- .travis.sh
+++ .travis.sh
@@ -9,9 +9,6 @@ php --version
   cd $(mktemp -d)
   curl https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 )
-if (hhvm --version | grep -q -- -dev); then
-  rm composer.lock
-fi
 composer install
 
 hh_client
