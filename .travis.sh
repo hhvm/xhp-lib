16235adcf3afa28336f1fbf4e203007ced445f4a
Fred Emmott <fe@fb.com>
2020-02-18 10:44:33 -0800

    [broken] Test with XHP namespace support enabled

--- .travis.sh
+++ .travis.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -ex
 apt update -y
 DEBIAN_FRONTEND=noninteractive apt install -y php-cli zip unzip
@@ -13,3 +13,13 @@ composer install
 
 hh_client
 vendor/bin/hacktest tests/
+
+if [ "$(hhvm --php -r 'var_dump(HHVM_VERSION_ID >= 44600);')" = "bool(true)" ]; then
+  hh_client stop
+  echo disable_xhp_element_mangling=true >> .hhconfig
+  echo enable_xhp_class_modifier=true >> .hhconfig
+  hh_client
+  rm -f ~/.hhvm.hhbc
+  hhvm -dhhvm.hack.lang.disable_xhp_element_mangling=true -dhhvm.hack.lang.enable_xhp_class_modifier=true vendor/bin/hh-autoload
+  hhvm -dhhvm.hack.lang.disable_xhp_element_mangling=true -dhhvm.hack.lang.enable_xhp_class_modifier=true vendor/bin/hacktest tests/
+fi
