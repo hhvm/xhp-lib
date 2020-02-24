dec676391c9169ebe0678505a2b0bb0959e56ae4
Fred Emmott <fe@fb.com>
2020-02-24 15:30:04 -0800

    some migrations needed before initial test run

--- .travis.sh
+++ .travis.sh
@@ -15,6 +15,12 @@ hh_client
 vendor/bin/hacktest tests/
 
 if [ "$(hhvm --php -r 'var_dump(HHVM_VERSION_ID >= 44600);')" = "bool(true)" ]; then
+  vendor/bin/hhast-migrate \
+    --demangle-xhp-class-names \
+    --add-xhp-children-declaration-method \
+    --remove-xhp-child-declarations \
+    src/ tests/
+
   hh_client stop
   echo disable_xhp_element_mangling=true >> .hhconfig
   echo enable_xhp_class_modifier=true >> .hhconfig
@@ -22,8 +28,8 @@ if [ "$(hhvm --php -r 'var_dump(HHVM_VERSION_ID >= 44600);')" = "bool(true)" ]; 
   hhvm -dhhvm.hack.lang.disable_xhp_element_mangling=true -dhhvm.hack.lang.enable_xhp_class_modifier=true vendor/bin/hh-autoload
   hhvm -dhhvm.hack.lang.disable_xhp_element_mangling=true -dhhvm.hack.lang.enable_xhp_class_modifier=true vendor/bin/hacktest tests/
 
-  vendor/bin/hhast-migrate --add-xhp-children-declaration-method --remove-xhp-child-declarations tests
-  vendor/bin/hhast-migrate --demangle-xhp-class-names --xhp-class-modifier tests
+  vendor/bin/hhast-migrate --xhp-class-modifier src tests
+
   hh_client
   hhvm -dhhvm.hack.lang.disable_xhp_element_mangling=true -dhhvm.hack.lang.enable_xhp_class_modifier=true vendor/bin/hh-autoload
   hhvm -dhhvm.hack.lang.disable_xhp_element_mangling=true -dhhvm.hack.lang.enable_xhp_class_modifier=true vendor/bin/hacktest tests/
