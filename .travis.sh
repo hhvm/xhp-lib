315627606308a44326dc6bd827bcafce0d978cd7
Fred Emmott <fe@fb.com>
2020-08-31 10:33:47 -0700

    Update minimum version, remove namespace-related options
    Default has flipped, these are no longer needed, and we want to test
    that everything works without them.
    
    Also removed the `|| true` from the composer install line - this was
    only needed as the autoloader failed due to missing options, which are
    no longer needed.

--- .travis.sh
+++ .travis.sh
@@ -9,9 +9,8 @@ php --version
   cd $(mktemp -d)
   curl https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 )
-composer install || true
+composer install
 
-hhvm -dhhvm.hack.lang.enable_xhp_class_modifier=true -dhhvm.hack.lang.disable_xhp_element_mangling=true vendor/bin/hh-autoload
 hh_client
-hhvm -dhhvm.hack.lang.enable_xhp_class_modifier=true -dhhvm.hack.lang.disable_xhp_element_mangling=true vendor/bin/hacktest tests/
-hhvm -dhhvm.hack.lang.enable_xhp_class_modifier=true -dhhvm.hack.lang.disable_xhp_element_mangling=true vendor/bin/hhast-lint
+hhvm vendor/bin/hacktest tests/
+hhvm vendor/bin/hhast-lint
